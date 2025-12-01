595d30a14faa8f5c079962078d85092719e178df
alvaro <alvaroch975@gmail.com>
2025-12-01 09:36:35 +0100

    v4 -> Final

--- src/app/component/contreras/service/tablon.ts
+++ src/app/component/contreras/service/tablon.ts
@@ -19,27 +19,27 @@ export class TablonService {
     if (direction === '') {
       direction = 'asc';
     }
-    return this.oHttp.get<IPage<ITablon>>(serverURL + `/recurso?page=${page}&size=${rpp}&sort=${order},${direction}`);
+    return this.oHttp.get<IPage<ITablon>>(serverURL + `/contreras?page=${page}&size=${rpp}&sort=${order},${direction}`);
   }
 
   get(id: number): Observable<ITablon> {
-    return this.oHttp.get<ITablon>(serverURL + '/recurso/' + id);
+    return this.oHttp.get<ITablon>(serverURL + '/contreras/' + id);
   }
 
   create(Tablon: Partial<ITablon>): Observable<number> {
-    return this.oHttp.post<number>(serverURL + '/recurso', Tablon);
+    return this.oHttp.post<number>(serverURL + '/contreras', Tablon);
   }
 
   update(Tablon: Partial<ITablon>): Observable<number> {
-    return this.oHttp.put<number>(serverURL + '/recurso', Tablon);
+    return this.oHttp.put<number>(serverURL + '/contreras', Tablon);
   }
 
   delete(id: number): Observable<number> {
-    return this.oHttp.delete<number>(serverURL + '/recurso/' + id);
+    return this.oHttp.delete<number>(serverURL + '/contreras/' + id);
   }
 
   rellenaTablon(numPosts: number): Observable<number> {
-    return this.oHttp.get<number>(serverURL + '/recurso/rellena/' + numPosts);
+    return this.oHttp.get<number>(serverURL + '/contreras/rellena/' + numPosts);
   }
 
 }
